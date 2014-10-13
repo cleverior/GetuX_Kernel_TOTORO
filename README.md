5d6e0ba70b598b3df030b7ef7793915a6c1080f3
Cleverior.ipul <dcleverior@gmail.com>
2014-10-14 05:43:38 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Source of GetuX Kernel for CyanogenMod 11 Totoro
+================================================
+
+Special Thanks to Allah SWT
+Credits :
+- Cleverior
+- Androidarmv6
+- Psyke83
+- and you (^_^)
+
+If you're using some of part/full from this source to make your own custom kernel, please include credits. Thanks.
---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Fri Oct 10 20:30:14 WIB 2014"
+#define LINUX_COMPILE_TIME "20:30:14"
+#define LINUX_COMPILE_BY "linmint"
+#define LINUX_COMPILE_HOST "linmint"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
